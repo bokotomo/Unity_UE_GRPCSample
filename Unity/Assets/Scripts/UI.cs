b3a6b2777db1aaf9f04319cf7a8dff2726fb551b
bokotomo <bokotomo@me.com>
2022-04-17 07:30:25 +0900

    feat: version up

---
+++ Unity/Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject testText = GameObject.Find("TestText");
+        testText.GetComponent<Text>().text = "tomomomo";
+        print("ok");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
